496d5271f487e579dd01f6022bdcfe9d74fb354f
vanelle-kenmogne <vanellahkens@gmail.com>
2022-09-10 04:48:09 -0700

    C language

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * * main-entry point for the program.
+ * *
+ * *Retrun:0 if no errors, return non zero if errors.
+ * */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
