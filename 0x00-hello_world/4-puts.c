900852698d2fd4f956f7a5d939122b8fe3f62f6e
vanelle-kenmogne <vanellahkens@gmail.com>
2022-09-12 00:44:02 -0700

    Puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
 /**
- * *main-serves as the entry point for the program
- * *
- * *Return: By default returns zero if no errors otherwise returns non-zero value.
- * */
+*main-serves as the entry point for the program
+*Return: By default returns zero if no errors otherwise returns non-zero value.
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
